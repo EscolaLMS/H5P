afc45a7b198003c3b8b9cb0852eecd216831f977
Mateusz <mateusz@qunabu.com>
2021-04-07 16:12:53 +0200

    cleanup, tests, envs gh actions

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,19 +4,24 @@ namespace EscolaLms\HeadlessH5P\Tests;
 
 use App\Models\User;
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
+use EscolaLms\HeadlessH5P\HeadlessH5PServiceProvider;
 
 class TestCase extends OrchestraTestCase
 {
     protected function setUp(): void
     {
         parent::setUp();
-        //$this->withFactories(__DIR__ . '/../../../database/factories');
-        //$this->withFactories(__DIR__ . '/../database/factories');
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [HeadlessH5PServiceProvider::class];
     }
 
     protected function getEnvironmentSetUp($app)
     {
         // Setup default database to use sqlite :memory:
         $app['config']->set('auth.providers.users.model', User::class);
+        $app['config']->set('passport.client_uuids', true);
     }
 }
