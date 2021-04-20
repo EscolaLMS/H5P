bbc005de346927f3c5ad638ec761903e7c45cfb2
Mateusz <mateusz@qunabu.com>
2021-04-20 11:50:33 +0200

    tests fo update

--- tests/Api/ContentApiTest.php
+++ tests/Api/ContentApiTest.php
@@ -8,13 +8,15 @@ use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
 use EscolaLms\HeadlessH5P\Tests\TestCase;
 use EscolaLms\HeadlessH5P\Models\H5PLibrary;
+use EscolaLms\HeadlessH5P\Models\H5PContent;
+
 //use Illuminate\Support\Facades\App;
 
 use EscolaLms\HeadlessH5P\Services\Contracts\HeadlessH5PServiceContract;
 
 class ContentApiTest extends TestCase
 {
-    public function test_library_create()
+    public function test_content_create()
     {
         $filename = 'arithmetic-quiz.h5p';
         $filepath = realpath(__DIR__.'/../mocks/'.$filename);
@@ -46,7 +48,7 @@ class ContentApiTest extends TestCase
         $response->assertJsonStructure(['id']);
     }
 
-    public function test_library_create_no_nonce()
+    public function test_content_create_no_nonce()
     {
         $library = H5PLibrary::first();
 
@@ -59,7 +61,7 @@ class ContentApiTest extends TestCase
         $response->assertStatus(422);
     }
 
-    public function test_library_create_invalid_library()
+    public function test_content_create_invalid_library()
     {
         $library = H5PLibrary::first();
 
@@ -73,7 +75,7 @@ class ContentApiTest extends TestCase
         $response->assertStatus(422);
     }
 
-    public function test_library_create_invalid_json()
+    public function test_content_create_invalid_json()
     {
         $library = H5PLibrary::first();
 
@@ -83,6 +85,75 @@ class ContentApiTest extends TestCase
             "library"=>$library->uberName,
             "params"=>"XXX!!!{\"params\":{\"taskDescription\":\"Documentation tool\",\"pagesList\":[{\"params\":{\"elementList\":[{\"params\":{},\"library\":\"H5P.Text 1.1\",\"metadata\":{\"contentType\":\"Text\",\"license\":\"U\",\"title\":\"Untitled Text\",\"authors\":[],\"changes\":[],\"extraTitle\":\"Untitled Text\"},\"subContentId\":\"da3387da-355a-49fb-92bc-3a9a4e4646a9\"}],\"helpTextLabel\":\"More information\",\"helpText\":\"\"},\"library\":\"H5P.StandardPage 1.5\",\"metadata\":{\"contentType\":\"Standard page\",\"license\":\"U\",\"title\":\"Untitled Standard page\",\"authors\":[],\"changes\":[],\"extraTitle\":\"Untitled Standard page\"},\"subContentId\":\"ac6ffdac-be02-448c-861c-969e6a09dbd5\"}],\"i10n\":{\"previousLabel\":\"poprzedni\",\"nextLabel\":\"Next\",\"closeLabel\":\"Close\"}},\"metadata\":{\"license\":\"U\",\"authors\":[],\"changes\":[],\"extraTitle\":\"fdsfds\",\"title\":\"fdsfds\"}}"
         ]);
+
+        $response->assertStatus(422);
+    }
+
+    ////
+
+    public function test_content_update()
+    {
+        $content = H5PContent::first();
+        $library = H5PLibrary::first();
+        $id = $content->id;
+
+        // TODO this should be from factory ?
+        $response = $this->postJson("/api/hh5p/content/$id", [
+          "nonce"=>bin2hex(random_bytes(4)),
+          "title"=>"The Title",
+          "library"=>$library->uberName,
+          "params"=>"{\"params\":{\"taskDescription\":\"Documentation tool\",\"pagesList\":[{\"params\":{\"elementList\":[{\"params\":{},\"library\":\"H5P.Text 1.1\",\"metadata\":{\"contentType\":\"Text\",\"license\":\"U\",\"title\":\"Untitled Text\",\"authors\":[],\"changes\":[],\"extraTitle\":\"Untitled Text\"},\"subContentId\":\"da3387da-355a-49fb-92bc-3a9a4e4646a9\"}],\"helpTextLabel\":\"More information\",\"helpText\":\"\"},\"library\":\"H5P.StandardPage 1.5\",\"metadata\":{\"contentType\":\"Standard page\",\"license\":\"U\",\"title\":\"Untitled Standard page\",\"authors\":[],\"changes\":[],\"extraTitle\":\"Untitled Standard page\"},\"subContentId\":\"ac6ffdac-be02-448c-861c-969e6a09dbd5\"}],\"i10n\":{\"previousLabel\":\"poprzedni\",\"nextLabel\":\"Next\",\"closeLabel\":\"Close\"}},\"metadata\":{\"license\":\"U\",\"authors\":[],\"changes\":[],\"extraTitle\":\"fdsfds\",\"title\":\"fdsfds\"}}"
+      ]);
+
+        if ($response->status() >= 422) {
+            echo $response->content();
+        }
+
+        $response->assertStatus(200);
+        $response->assertJsonStructure(['id']);
+    }
+
+    public function test_content_update_no_nonce()
+    {
+        $content = H5PContent::first();
+        $id = $content->id;
+
+        $response = $this->postJson("/api/hh5p/content/$id", [
+            "title"=>"The Title",
+          "library"=>"Invalid lib name",
+          "params"=>"{\"params\":{\"taskDescription\":\"Documentation tool\",\"pagesList\":[{\"params\":{\"elementList\":[{\"params\":{},\"library\":\"H5P.Text 1.1\",\"metadata\":{\"contentType\":\"Text\",\"license\":\"U\",\"title\":\"Untitled Text\",\"authors\":[],\"changes\":[],\"extraTitle\":\"Untitled Text\"},\"subContentId\":\"da3387da-355a-49fb-92bc-3a9a4e4646a9\"}],\"helpTextLabel\":\"More information\",\"helpText\":\"\"},\"library\":\"H5P.StandardPage 1.5\",\"metadata\":{\"contentType\":\"Standard page\",\"license\":\"U\",\"title\":\"Untitled Standard page\",\"authors\":[],\"changes\":[],\"extraTitle\":\"Untitled Standard page\"},\"subContentId\":\"ac6ffdac-be02-448c-861c-969e6a09dbd5\"}],\"i10n\":{\"previousLabel\":\"poprzedni\",\"nextLabel\":\"Next\",\"closeLabel\":\"Close\"}},\"metadata\":{\"license\":\"U\",\"authors\":[],\"changes\":[],\"extraTitle\":\"fdsfds\",\"title\":\"fdsfds\"}}"
+      ]);
+
+        $response->assertStatus(422);
+    }
+
+    public function test_content_update_invalid_library()
+    {
+        $content = H5PContent::first();
+        $id = $content->id;
+
+        $response = $this->postJson("/api/hh5p/content/$id", [
+            "nonce"=>bin2hex(random_bytes(4)),
+          "title"=>"The Title",
+          "library"=>"Invalid lib name",
+          "params"=>"{\"params\":{\"taskDescription\":\"Documentation tool\",\"pagesList\":[{\"params\":{\"elementList\":[{\"params\":{},\"library\":\"H5P.Text 1.1\",\"metadata\":{\"contentType\":\"Text\",\"license\":\"U\",\"title\":\"Untitled Text\",\"authors\":[],\"changes\":[],\"extraTitle\":\"Untitled Text\"},\"subContentId\":\"da3387da-355a-49fb-92bc-3a9a4e4646a9\"}],\"helpTextLabel\":\"More information\",\"helpText\":\"\"},\"library\":\"H5P.StandardPage 1.5\",\"metadata\":{\"contentType\":\"Standard page\",\"license\":\"U\",\"title\":\"Untitled Standard page\",\"authors\":[],\"changes\":[],\"extraTitle\":\"Untitled Standard page\"},\"subContentId\":\"ac6ffdac-be02-448c-861c-969e6a09dbd5\"}],\"i10n\":{\"previousLabel\":\"poprzedni\",\"nextLabel\":\"Next\",\"closeLabel\":\"Close\"}},\"metadata\":{\"license\":\"U\",\"authors\":[],\"changes\":[],\"extraTitle\":\"fdsfds\",\"title\":\"fdsfds\"}}"
+      ]);
+
+        $response->assertStatus(422);
+    }
+
+    public function test_content_update_invalid_json()
+    {
+        $library = H5PLibrary::first();
+        $content = H5PContent::first();
+        $id = $content->id;
+
+        $response = $this->postJson("/api/hh5p/content/$id", [
+            "nonce"=>bin2hex(random_bytes(4)),
+          "title"=>"The Title",
+          "library"=>$library->uberName,
+          "params"=>"XXX!!!{\"params\":{\"taskDescription\":\"Documentation tool\",\"pagesList\":[{\"params\":{\"elementList\":[{\"params\":{},\"library\":\"H5P.Text 1.1\",\"metadata\":{\"contentType\":\"Text\",\"license\":\"U\",\"title\":\"Untitled Text\",\"authors\":[],\"changes\":[],\"extraTitle\":\"Untitled Text\"},\"subContentId\":\"da3387da-355a-49fb-92bc-3a9a4e4646a9\"}],\"helpTextLabel\":\"More information\",\"helpText\":\"\"},\"library\":\"H5P.StandardPage 1.5\",\"metadata\":{\"contentType\":\"Standard page\",\"license\":\"U\",\"title\":\"Untitled Standard page\",\"authors\":[],\"changes\":[],\"extraTitle\":\"Untitled Standard page\"},\"subContentId\":\"ac6ffdac-be02-448c-861c-969e6a09dbd5\"}],\"i10n\":{\"previousLabel\":\"poprzedni\",\"nextLabel\":\"Next\",\"closeLabel\":\"Close\"}},\"metadata\":{\"license\":\"U\",\"authors\":[],\"changes\":[],\"extraTitle\":\"fdsfds\",\"title\":\"fdsfds\"}}"
+      ]);
 
         $response->assertStatus(422);
     }
