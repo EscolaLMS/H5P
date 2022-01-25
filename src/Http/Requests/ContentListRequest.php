9cd133ef5d45f272cc74c62aaf28e34ac5360088
Mateusz Qunabu <mateusz@qunabu.com>
2022-01-25 15:05:28 +0100

    fixinf gate (#75)
    Co-authored-by: Mateusz Wojczal <mateuszwojczal@Mateuszs-MacBook-Pro.local>

--- src/Http/Requests/ContentListRequest.php
+++ src/Http/Requests/ContentListRequest.php
@@ -10,7 +10,7 @@ class ContentListRequest extends FormRequest
 {
     public function authorize(): bool
     {
-        return true;//Gate::allows('list', H5PContent::class);
+        return Gate::allows('list', H5PContent::class);
     }
 
     public function rules(): array
