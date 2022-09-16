49796e387124c73a6dff43f3cfe1a887aac0595b
Tomasz Smolarek <tomasz.smolarek@escolasoft.com>
2022-09-16 16:06:58 +0200

    title fix

--- database/seeders/ContentLibrarySeeder.php
+++ database/seeders/ContentLibrarySeeder.php
@@ -42,6 +42,7 @@ class ContentLibrarySeeder extends Seeder
         $h5p_upgrade_only = false;
 
         if ($this->hh5pService->getValidator()->isValidPackage($skipContent, $h5p_upgrade_only)) {
+            $this->hh5pService->getRepository()->setMainData($this->hh5pService->getCore()->mainJsonData);
             $this->hh5pService->getStorage()->savePackage($content, null, $skipContent);
         } else {
             echo "Invalid package $filename \n";
